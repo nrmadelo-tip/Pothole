526f5e70dfc940b6582a0aac41250d8ece7cb483
nrmadelo-tip <75427111+nrmadelo-tip@users.noreply.github.com>
2022-10-25 19:46:20 +0800

    NEW
    BAGO ULIT

--- pages/LogScreen.js
+++ pages/LogScreen.js
@@ -1,4 +1,4 @@
-import { FlatList,Image, StyleSheet, Text, View } from 'react-native'
+import { FlatList,Image, StyleSheet, Text, View, ScrollView } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context';
 
@@ -46,42 +46,36 @@ const Log = () => {
       <View style={styles.headerBar}>
           <Text style={styles.txtBar}> Log Screen</Text>
       </View>
-      <ScrollView
-          
-      >
           <View>
               <Text style={styles.txtHeader}>List of Potholes</Text>
           </View>
-          <View>
-                <FlatList 
-                contentContainerStyle={{
-                  padding : 20,
-                  backgroundColor: "white"
-                }}
+          <FlatList 
+                // contentContainerStyle={{
+                //   padding : 20,
+                //   backgroundColor: "white"
+                // }}
                 data={data}
                 keyExtractor={({id}, index) => id}
-                renderItem= {({id})=>(
-
+                contentContainerStyle={{ paddingBottom: "20%"}}
+                renderItem= {({item}) =>(
+          <View>
+                
                     <View style={styles.itemList}>
-                      <View/>
-                      <View style={styles.icon}/>
-
                       <Image 
-                      source={{uri: `data:image/png;base64,${item.blobData}`}} 
-                      style={{width: 100, height: 100, margin:5}}/>
+                      source={{uri: `data:image/png;base64,${item.blobData}`}}
+                      style={{width: 200, height: 100, margin:5}}/>
                       
                       <View style={{paddingLeft:15}}>
-                        <Text style={styles.txtName}>ID: {item.id}</Text>
                         <Text style={styles.txtName}>Location: {item.location}</Text>
                         <Text style={styles.txtName}>dateTime: {item.dateTime}</Text>
                         <Text style={styles.txtName}>Latitude: {item.latitude}</Text>
-                        <Text style={styles.txtName}>Longitude: {item.longitude}</Text>
+                        <Text style={styles.txtName}>Longtitude: {item.longitude}</Text>
                       </View>
                     </View> 
-                )}/>
+                
           </View>
+          )}/>
           <View style={styles.headerBar}/>
-        </ScrollView>
     </SafeAreaView>
   );
 };
@@ -99,25 +93,24 @@ const styles = StyleSheet.create({
   txtBar:{
     fontsize: 50,
     fontWeight: "bold",
+    textAlign: "center",
   },
   txtHeader:{
     fontsize: 18,
-    fontWeight: "bold"
+    fontWeight: "bold",
+    textAlign: "center"
   },
   itemList:{
     paddingVertical: 15,
     borderBottomColor: "#e2e2e",
     borderBottomWidth: 0.5,
-    flexDirection: 'row'
+    justifyContent: 'center',
+    alignItems: 'center'
+    // flexDirection: 'row',
   },
   txtName:{
     fontsize: 16,
+    textAlign: "center",
     fontWeight: "bold"
   },
-  icon:{
-    width:150,
-    height:150,
-    margin:5,
-    backgroundColor: "#e6e6e6"
-  }
 });
