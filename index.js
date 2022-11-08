e13e90d9630ff64500d10c816d2a7afb9f76ebf0
nrmadelo-tip <75427111+nrmadelo-tip@users.noreply.github.com>
2022-11-09 04:46:45 +0800

    Notification Fix

--- index.js
+++ index.js
@@ -5,7 +5,57 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import PushNotification from "react-native-push-notification";
+
+import PushNotification, {Importance} from "react-native-push-notification";
+
+
+
+// Must be outside of any component LifeCycle (such as componentDidMount).
+PushNotification.configure({
+    // (optional) Called when Token is generated (iOS and Android)
+    onRegister: function (token) {
+      //console.log("TOKEN:", token);
+    },
+
+    // (required) Called when a remote is received or opened, or local notification is opened
+    onNotification: function (notification) {
+      //console.log("NOTIFICATION:", notification);
+
+      // process the notification
+
+      // (required) Called when a remote is received or opened, or local notification is opened
+      //notification.finish(PushNotificationIOS.FetchResult.NoData);
+    },
+
+    // IOS ONLY (optional): default: all - Permissions to register.
+    permissions: {
+      alert: true,
+      badge: true,
+      sound: true,
+    },
+
+    popInitialNotification: true,
+    requestPermissions: true,
+  });
+
+
+
+
+PushNotification.createChannel(
+    {
+      channelId: "channel-id", // (required)
+      channelName: "My channel", // (required)
+      channelDescription: "A channel to categorise your notifications", // (optional) default: undefined.
+      playSound: false, // (optional) default: true
+      soundName: "default", // (optional) See `soundName` parameter of `localNotification` function
+      importance: Importance.HIGH, // (optional) default: Importance.HIGH. Int value of the Android notification importance
+      vibrate: true, // (optional) default: true. Creates the default vibration pattern if true.
+    },
+    (created) => console.log(`createChannel returned '${created}'`) // (optional) callback returns whether the channel was created, false means it already existed.
+  );
+
+
+
 
 
 
